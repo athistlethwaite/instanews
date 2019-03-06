9116cdcc0a8d99c3a29dc8f133bdd4962bfc8b7e
Alysha Thistlethwaite <alysha.thistlethwaite@gmail.com>
2019-03-06 14:43:27 -0800

    Updated readme.md

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 $(function () {
   //1. Listen for "Select..."
   $('#drop-down').on('change', function (event) {
-    event.preventDefault()
-
+    event.preventDefault();
     const section = $('#drop-down').val();
-
-    $("header").addClass("header-small");
+    $(".site-header").addClass("small-header");
 
     if (section !== "") {
       $(".ajax-loader").show();
@@ -14,46 +12,35 @@ $(function () {
   });
 
   function getStories(section) {
-    $('.top-stories ul').empty();
+    $('.top-stories ul').empty(); //2. Make ajax request: 
 
-    //2. Make ajax request: 
     $.ajax({
         method: 'GET',
         url: 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json?api-key=g6kpkfDKZ7PlqdA4ds2KEggP82QRbUyZ',
-        dataType: 'json',
-      })
-      .done(function (data) {
-          if (data.results.length === 0) {
-            $('.top-stories ul').append('<li> Please select another section. </li>')
-
-          } else {
-            const filteredArticles = data.results.filter(function (value) {
-              return value.multimedia[4] !== undefined
-            })
-            const articles = filteredArticles.slice(0, 12);
-
-            $.each(articles, function (key, value) {
-              const abstract = '<p class="abstract">' + value.abstract + '</p>';
-              let img = '<div class="article-image" style="background-image:url(' + value.multimedia[4].url + ');">' + abstract + '</div>';
-              const url = value.url
-
-              console.log(value);
-
-              $(".top-stories ul").append('<li class="article-list">' + '<a href="' + url + '">' + img + "</a>" + "</li>")
-
-            }))
+        dataType: 'json'
+      }).done(function (data) {
+        if (data.results.length === 0) {
+          $('.top-stories ul').append('<li> Please select another section. </li>');
+        } else {
+          const filteredArticles = data.results.filter(function (value) {
+            return value.multimedia[4] !== undefined;
+          });
+          const articles = filteredArticles.slice(0, 12);
+          console.log(articles)
+          $.each(articles, function (key, value) {
+            const abstract = '<p class="abstract">' + value.abstract + '</p>';
+            let img = '<div class="article-image" style="background-image:url(' + value.multimedia[4].url + ');">' + abstract + '</div>';
+            const url = value.url;
+            console.log(value);
+            $(".top-stories ul").append('<li class="article-list">' + '<a href="' + url + '">' + img + "</a>" + "</li>");
+          });
         }
-      })
-
-  //3. If unsuccessful 
-  .fail(function () {
-      console.log("Sorry, no articles were found. Please select another section.");
-    })
-
-    //4. Hide loader
-    .always(function () {
-      $('.ajax-loader').hide();
-    })
-}
-
+      }) //3. If unsuccessful 
+      .fail(function () {
+        console.log("Sorry, no articles were found. Please select another section.");
+      }) //4. Hide loader
+      .always(function () {
+        $('.ajax-loader').hide();
+      });
+  }
 });
